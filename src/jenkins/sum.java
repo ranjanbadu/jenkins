4b66490fd6f64207b7db4008b850309792381810
ranjanbadu <54083685+ranjanbadu@users.noreply.github.com>
2020-07-31 01:27:58 +0530

    new added

--- src/jenkins/sum.java
+++ src/jenkins/sum.java
@@ -1,10 +1,27 @@
 package jenkins;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map.Entry;
+
 public class sum {
 	public static void main (String[]args) {
-	int num=50;
-	int sum =60;
-	System.out.println(num+sum);
-	System.out.println("add me");
+		String str []= {"aa","bb","cc","dd"};
+		int num []= {10,20,30,40};
+		HashMap<String,Integer>st = new HashMap<>();
+		for(String count:str) {
+			if(!st.containsKey(count)) {
+				st.put(count, 1);
+			}
+			else {
+				
+			}
+		}
+	for(Entry<String,Integer>it:st.entrySet()) {
+		String name= it.getKey();
+		int value=it.getValue();
+		System.out.println(name+" "+value);
+	}
 }
 }
